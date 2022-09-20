22dc4e4a8ed8be787ba95a3b3568b883aa70a899
NaveedMohammed <rmdnaveed@gmail.com>
2022-09-20 15:51:54 -0500

    Server is now hosting content from the Vue app

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 port = 3000;
 
+server.set('view engine', 'html');
+server.use(express.static(path.join(__dirname, 'frontEnd/dist')))
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({
   extended: false
 }));
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+server.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontEnd/dist/index.html'));
 })
 
 server.listen(port, () => {
