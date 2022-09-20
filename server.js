7aff6f4b52212d4dfd33a8e6bea8d8b3089dbc3d
NaveedMohammed <rmdnaveed@gmail.com>
2022-09-20 16:28:01 -0500

    Added mongodb connection Close #4

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-port = 3000;
+const mongoose = require('mongoose');
+port = process.env.PORT || 8203;
+mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/c2stem-CMISE';
+
+mongoose.connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MONGO CONNECTION OPEN!")
+}).catch(err => {
+    console.log("MONGO CONNECTION ERROR!!")
+    console.log(err)
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 server.set('view engine', 'html');
 server.use(express.static(path.join(__dirname, 'frontEnd/dist')))
