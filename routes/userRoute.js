9b88f972b52a3ed6f514ce9e508e45265ef20515
NaveedMohammed <rmdnaveed@gmail.com>
2022-12-07 01:53:28 -0600

    Added user authentication and http communication

---
+++ routes/userRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+
+router.post('/login', catchAsync(async (req, res, next) => {
+
+    return passport.authenticate('local', { session: false }, (err, user, _info) => {
+        if(err) {
+          return next(err);
+        }
+    
+        if(user) {
+            const token = user.generateJwt();
+            return res.status(200).json({ token });
+        }
+    
+        return res.status(400).info;
+      })(req, res, next);
+}));
+
+module.exports = router;
