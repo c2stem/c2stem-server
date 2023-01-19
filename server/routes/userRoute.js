5372be1e3837e7c17208bec05b6a4187305ffbf4
NaveedMohammed <rmdnaveed@gmail.com>
2023-01-19 13:39:04 -0600

    Added regiser route and functionality

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,26 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const User = require('../models/user');
-const catchAsync = require('../utils/CatchAsync');
+const passport = require("passport");
+const User = require("../models/user");
+const catchAsync = require("../utils/CatchAsync");
 
-router.post('/login', catchAsync(async (req, res, next) => {
-    return passport.authenticate('local', { session: false }, (err, user, _info) => {
-        if(err) {
+router.post(
+  "/login",
+  catchAsync(async (req, res, next) => {
+    return passport.authenticate(
+      "local",
+      { session: false },
+      (err, user, _info) => {
+        if (err) {
           console.log("error while authenticating", err);
           return next(err);
         }
-    
-        if(user) {
-            const token = user.generateJwt();
-            const userRole = user.role;
-            const userClass = user.class;
-            console.log("passed auth", token, user);
-            return res.status(200).json({ token: token, role: userRole, class: userClass });
+
+        if (user) {
+          const token = user.generateJwt();
+          const userRole = user.role;
+          const userClass = user.class;
+          return res
+            .status(200)
+            .json({ token: token, role: userRole, class: userClass });
         }
-    
+
         return res.status(400).info;
-      })(req, res, next);
-}));
+      }
+    )(req, res, next);
+  })
+);
+
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    const user = new User();
+
+    user.username = req.body.username;
+    user.email = req.body.email;
+    user.class = req.body.class;
+    user.role = req.body.role;
+    user.setPassword(req.body.password);
+    user.save((err) => {
+      if (err) {
+        res.status(400).json(err);
+        return;
+      }
+
+      const token = user.generateJwt();
+      res.status(200).json({ token });
+    });
+  })
+);
 
 module.exports = router;
