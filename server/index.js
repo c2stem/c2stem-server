98a0e65a70d80ffaac0722d3fae9000f6bfd82f1
NaveedMohammed <rmdnaveed@gmail.com>
2023-03-07 14:12:07 -0600

    Changed cors parameters

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ server.use(bodyParser.urlencoded({
 server.use(passport.initialize());
 
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
