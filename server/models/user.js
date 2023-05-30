1dea79f790341b37327e5ca6ecd84936d689af3d
NaveedMohammed <rmdnaveed@gmail.com>
2023-05-30 17:21:37 -0500

    Added experimental group to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,57 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new Schema ({
-    email: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true, 
-        default: 'User'
-    },
-    class: {
-        type: String,
-        required: true,
-        default: 'SPICE'
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    hash: String,
-    salt: String
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "User",
+  },
+  class: {
+    type: String,
+    required: true,
+    default: "SPICE",
+  },
+  group: {
+    type: String,
+    required: true,
+    default: "CMISE",
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  hash: String,
+  salt: String,
 });
 
 userSchema.methods.setPassword = function (password) {
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(
-        password, this.salt, 1000, 64, 'sha1'
-    ).toString('hex');
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha1")
+    .toString("hex");
 };
 
 userSchema.methods.validPassword = function (password) {
-    const hash = crypto.pbkdf2Sync(
-        password, this.salt, 1000, 64, 'sha1'
-    ).toString('hex');
-    return this.hash === hash;
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha1")
+    .toString("hex");
+  return this.hash === hash;
 };
 
 userSchema.methods.generateJwt = function () {
-    var expiry = new Date();
-    expiry.setDate(expiry.getDate() + 7);
-    return jwt.sign({
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        role: this.role,
-        class: this.class,
-        exp: parseInt(expiry.getTime() / 1000)
-    }, process.env.JWT_SECRET_KEY);
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      role: this.role,
+      class: this.class,
+      exp: parseInt(expiry.getTime() / 1000),
+    },
+    process.env.JWT_SECRET_KEY
+  );
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
---
+++ server/test/user.test.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const crypto = require("crypto");
+
+mongo_uri = "mongodb://localhost:27017/c2stem-class";
+
+mongoose
+  .connect(mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!");
+  })
+  .catch((err) => {
+    console.log("MONGO CONNECTION ERROR!!");
+    console.log(err);
+  });
+
+const password = "test";
+const userData = {
+  email: "test@test.com",
+  role: "user",
+  class: "CMISE",
+  group: "IE",
+  username: "test",
+};
+
+describe("User model", () => {
+  it("create and save user successfull", async () => {
+    const newUser = new User(userData);
+    await newUser.setPassword(password);
+    const savedUser = await newUser.save();
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.email).toBe(userData.email);
+    expect(savedUser.username).toBe(userData.username);
+    expect(savedUser.role).toBe(userData.role);
+    expect(savedUser.class).toBe(userData.class);
+    expect(savedUser.group).toBe(userData.group);
+    expect(savedUser.salt).toBeDefined();
+  });
+});
